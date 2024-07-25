fca484735cbe662e31edc10192cb37d4dba4457d
kirtisoglu <77515254+kirtisoglu@users.noreply.github.com>
2024-07-25 11:05:23 -0500

    added new constraints

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
-# Directed Reading Program (DRP) Mentor-Mentee Matching Problem
+# Directed Reading Program (DRP) Matching Model
 
 ### Description
 The Directed Reading Program Matching Model is a Python-based solution designed to automate the matching process between mentees and mentors for the [DRP Türkiye](https://sites.google.com/view/drp-turkey/) in Türkiye.
 
 Using the PuLP library, a linear programming model is constructed that provides matching according to certain criteria. This model aims to maximize the overall match quality based on interests, ensuring each mentee is paired with a suitable mentor while adhering to capacity constraints.
 
-### Linear Programming Model
-An optimum solution of a maximization model is a vector x that satisfies all the constraints and has the maximum objective value f(x). We use the model explained in the Model.pdf file.
+### About the Linear Programming 
+An optimum solution of a minimization model is a vector x that satisfies all the constraints, and has the minimum objective value f(x). ( x does not have to be unique.) Luckily, we do not need to find the best solution: who cares if we have a perfect matching of mentors & mentees, it is enough for us to find a “good” matching. For this reason, we might use a metaheuristic algorithm to solve our model: 
 
 ### DRP Türkiye
 DRP Türkiye is an online program that pairs undergraduate students studying mathematics at universities in and around Türkiye with graduate students and young researchers at institutions around the world to work together on selected books or articles in mathematics during the summer.
 To check out more details, please see the [Previous Programs](https://sites.google.com/view/drp-turkey/previous-programs?authuser=0) and [DRP Türkiye YouTube Channel](https://www.youtube.com/@drpturkey2583)
-
 ## Prerequisites
-This project is implemented on Python 3.8+. You will also need to install the required libraries:
+
+Before you use the DRP Matching Model, ensure you have Python installed on your system. This project is tested on Python 3.8+. You will also need to install the required libraries:
 
 ```bash
 pip install pulp pandas
 ```
 * The model is returned as output as a dictionary and pandas DataFrame containing scores with each mentor and matched mentee(s).
 
-## Data
-To create an input file conforming to the scheme used by the program, you should use a data frame with the following format.
+## Schema of the CSV File
+To create an input file conforming to the schema used by the program,
+you should use the following format:
 
 ### Columns for Students and Mentors
 *Name*<br>
@@ -31,7 +32,7 @@ The full name of the applicant
 Gender of the applicant
 
 *University*<br>
-The University of the student
+The university of the student
 
 *Department*<br>
 The department of the applicant
@@ -63,4 +64,9 @@ General topology, Geometric topology, Algebraic topology, Algebraic geometry, Di
 
 *Applied Mathematics*<br>
 Probability theory, Statistics, Computer algebra, Data science, Cryptography/Coding theory, Mathematical modeling, Classical mechanics, Complex/Dynamical systems, Quantum theory, Relativity and Quantum relativistic theories, Statistical mechanics
+
+
+
+
+
 
--- model.py
+++ model.py
@@ -1,9 +1,105 @@
+
+
+# Summary Example
+""" MAIN FUNCTION: Bipartition of Tree
+    This function finds a balanced 2 partition of a graph by drawing a
+    spanning tree and finding an edge to cut that leaves at most an epsilon
+    imbalance between the populations of the parts. If a root fails, new roots
+    are tried until node_repeats in which case a new tree is drawn.
+
+    Builds up a connected subgraph with a connected complement whose population
+    is ``epsilon * pop_target`` away from ``pop_target``."""
+
+
+"""Key functionalities include:
+
+- Predecessor and successor functions for graph traversal using breadth-first search.
+- Implementation of random and uniform spanning trees for graph partitioning.
+
+Dependencies:
+
+- networkx: Used for graph data structure and algorithms.
+- random: Provides random number generation for probabilistic approaches.
+- typing: Used for type hints.
+
+Last Updated: March 22, 2024
+"""
+
+
 from pulp import LpProblem, LpVariable, lpSum, LpMaximize, LpBinary, PULP_CBC_CMD
+import pandas as pd
+
+
+# Use 'functools' (import partial) and 'typing' libraries to define the following function
+def matching_one(mentee_choice, mentor_choice, uni_students, scores, max_student, uni_capacity, time):
+
+    # intro Example
+    """
+    Builds a spanning tree chosen by Kruskal's method using random weights.
+
+    :param graph: The input graph to build the spanning tree from. Should be a Networkx Graph.
+    :type graph: nx.Graph
+    :param weight_dict: Dictionary of weights to add to the random weights used in region-aware
+    variants.
+    :type weight_dict: Optional[Dict], optional
+
+    :returns: The maximal spanning tree represented as a Networkx Graph.
+    :rtype: nx.Graph
+    """
+
+    #Initialize a minimization problem
+    problem = LpProblem("Mentor_Mentee_Matching", LpMaximize)
+
+    # Create decision variable x_ij for every mentee i and mentor j
+    decision = {(i, j): LpVariable(f"x_{i}_{j}", cat="Binary") for i in mentee_choice.keys() for j in mentor_choice.keys()}
+
+    # Constraint (2): each mentor receives at least one mentees, at most 'max_student' mentees. 
+    for j in mentor_choice.keys():
+        problem += lpSum(decision[i, j] for i in mentee_choice.keys()) >= 1
+        problem += lpSum(decision[i, j] for i in mentee_choice.keys()) <= max_student
+
+    # Constraint (3): each mentee is assigned to at most one mentor.
+    for i in mentee_choice.keys():
+        problem += lpSum(decision[i, j] for j in mentor_choice.keys()) <= 1
+
+    # Constraint (4): at most 'uni_capacity' mentees are assigned from every uni.
+    for uni in uni_students.keys():
+        problem += lpSum(decision[i, j] for i in uni_students[uni] for j in mentor_choice.keys()) <= uni_capacity
+
+    # Constraint (5): mentee i is assigned to mentor j only if their interests overlap.
+    for i in mentee_choice.keys():
+        for j in mentor_choice.keys():
+            problem += decision[i, j] <= scores[(i,j)]
+
+    # Objective function: the total assignment score is maximized.
+    problem += lpSum(decision[i, j]*scores[(i,j)] for i in mentee_choice.keys() for j in mentor_choice.keys())
+
+    # Solve the problem
+    problem.solve(PULP_CBC_CMD(msg=0, timeLimit=time))
+
+    # Solution
+    solution = {'Mentor' : [], 'Student' : [], 'Score' : []}
+    for j in mentor_choice.keys():
+        for i in mentee_choice.keys():
+            if decision[i, j].varValue == 1:
+                solution['Mentor'].append(j)
+                solution['Student'].append(i)
+                solution['Score'].append(scores[(i,j)])
+                                 
+    df = pd.DataFrame(data = solution)
+
+    return problem.objective.value(), solution, df
 
 
 
-def matching(mentee_choice, mentor_choice, uni_students, scores, max_student, uni_capacity, time):
+def matching_two(mentee_choice, mentor_choice, uni_students, mentee_high, mentor_high, scores, gender, ref_score, max_student, 
+                 uni_capacity, time, epsilon, min_ref, weight_subject, weight_class, weight_ref):
 
+    """
+    Model 2: Constraints (6) and (7) are added. Objective function is changed. The rest is the same as Model 1.
+        
+        will be completed later on
+    """
 
     #Initialize a minimization problem
     problem = LpProblem("Mentor_Mentee_Matching", LpMaximize)
@@ -32,22 +128,36 @@ def matching(mentee_choice, mentor_choice, uni_students, scores, max_student, un
     # Constraint (5): mentee i is assigned to mentor j only if their interests overlap.
     for i in mentee_choice.keys():
         for j in mentor_choice.keys():
-            problem += decision[i, j] <= decision[i, j]*scores[(i,j)]
+            problem += decision[i, j] <= scores[(i,j)]
 
 
-    # Objective function: the total assignment score is maximized.
-    problem += lpSum(decision[i, j]*scores[(i,j)] for i in mentee_choice.keys() for j in mentor_choice.keys())
+    # Constraint (6): gender equality.
+    problem += lpSum(decision[i, j]*gender[i] for i in mentee_choice.keys() for j in mentor_choice.keys()) >= (1 - epsilon) * lpSum(decision[i, j] for i in mentee_choice.keys() for j in mentor_choice.keys())
+        
+    
+    # Constraint (7): eliminates mentees with ref score below r.
+    for i in mentee_choice.keys():
+        for j in mentor_choice.keys():
+            problem += min_ref * decision[i, j] <= ref_score[i]
+    
+
+    # Objective function: sum od assignment scores is maximized.
+    problem += weight_subject *lpSum(decision[i, j]*score[(i,j)] for i in mentee_choice.keys() for j in mentor_choice.keys()) + weight_class * lpSum(decision[i, j] for i in mentee_high for j in mentor_high) + weight_ref * lpSum(decision[i, j]*ref_score[i] for i in mentee_choice.keys() for j in mentor_choice.keys())
 
     # Solve the problem
     problem.solve(PULP_CBC_CMD(msg=0, timeLimit=time))
 
 
     # Solution
-    solution = {}
-    for i in mentee_choice.keys():
-        for j in mentor_choice.keys():
+    solution = {'Mentor' : [], 'Student' : [], 'Score' : []}
+    for j in mentor_choice.keys():
+        for i in mentee_choice.keys():
             if decision[i, j].varValue == 1:
-                solution[(i, j)] = scores[(i,j)]
+                solution['Mentor'].append(j)
+                solution['Student'].append(i)
+                solution['Score'].append(score[(i,j)])
+                                 
+    df = pd.DataFrame(data = solution)
 
 
-    return problem.objective.value(), solution
+    return problem.objective.value(), solution, df
